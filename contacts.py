217ac459f6a52ec8ecb8018a782ef28b0d7da7ac
Ondřej Gajdušek <ogajduse@redhat.com>
2019-04-06 22:54:43 +0200

    Complete API for inserting data

---
+++ contacts.py
@@ -0,0 +1,39 @@
+"""
+Module to handle /contacts API calls.
+"""
+
+from jsonschema import validate
+
+
+class ContactsNewAPI:
+    """ Main /packages API class."""
+
+    def __init__(self, my_db):
+        self.my_db = my_db
+        self.json_schema = {
+            'owner_id': 'owner_id',
+            'user_id': 'user_id',
+            'encrypted_alias': 'alias'
+        }
+
+    async def process_post(self, api_version, data):  # pylint: disable=unused-argument
+        """
+        Process inserting new contact to the database.
+        :param data: json request parsed into data structure
+        :returns: json response with inserted contact
+        """
+        validate(data, self.json_schema)
+
+        owner_id = data.get('owner_id')
+        user_id = data.get('user_id')
+        encrypted_alias = data.get('encrypted_alias')
+
+        await self.my_db.insert_contact(owner_id, user_id, encrypted_alias)
+
+        response = {
+            'owner_id': owner_id,
+            'user_id': user_id,
+            'encrypted_alias': encrypted_alias,
+        }
+
+        return response
