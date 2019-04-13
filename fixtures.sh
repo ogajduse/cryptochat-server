7c946a87b3acc0a8b7f1ec31ab7e2615f10bd1a2
Ondřej Gajdušek <ogajduse@redhat.com>
2019-04-13 22:55:53 +0200

    change fixture with respect to api changes

--- fixtures.sh
+++ fixtures.sh
@@ -1,26 +1,26 @@
 # insert user1
-curl http://localhost:8888/api/users -X 'POST' -H 'Content-Type: application-json' --data '{"user_id": 123, "public_key":"public_key_data"}'
+curl http://localhost:8888/api/users -X 'POST' -H 'Content-Type: application-json' --data '{"user_id": 123, "public_key":"public_key_data"}' | jq .
 
 # insert user2
-curl http://localhost:8888/api/users -X 'POST' -H 'Content-Type: application-json' --data '{"user_id": 123, "public_key":"public_key_data2"}'
+curl http://localhost:8888/api/users -X 'POST' -H 'Content-Type: application-json' --data '{"user_id": 123456, "public_key":"public_key_data2"}' | jq .
 
 # insert contact1
-curl http://localhost:8888/api/contacts -X 'POST' -H 'Content-Type: application-json' --data '{"owner_id": 123, "user_id": 123456, "encrypted_alias": "USER2 in contacts of USER1"}'
+curl http://localhost:8888/api/contacts -X 'POST' -H 'Content-Type: application-json' --data '{"owner_id": 123, "user_id": 123456, "encrypted_alias": "USER2 in contacts of USER1"}' | jq .
 
 # insert contact2
-curl http://localhost:8888/api/contacts -X 'POST' -H 'Content-Type: application-json' --data '{"owner_id": 123456, "user_id": 123, "encrypted_alias": "USER1 in contacts of USER2"}'
+curl http://localhost:8888/api/contacts -X 'POST' -H 'Content-Type: application-json' --data '{"owner_id": 123456, "user_id": 123, "encrypted_alias": "USER1 in contacts of USER2"}' | jq .
 
 # insert chat1
-curl http://localhost:8888/api/chats -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "owner": 123, "users": [123, 123456], "users_public_key": ["pkenc_data", "pkenc_data2"]}'
+curl http://localhost:8888/api/chats -X 'POST' -H 'Content-Type: application-json' --data '{"users": [123, 123456], "sym_key_enc_by_owners_pub_keys": ["pkenc_data", "pkenc_data2"]}' | jq .
 
 # insert message
-curl http://localhost:8888/api/message/new -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "sender_id": 123, "message": "Hi there!"}'
+curl http://localhost:8888/api/message/new -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "sender_id": 123, "message": "Hi there!"}' | jq .
 
 # insert message
-curl http://localhost:8888/api/message/new -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "sender_id": 123456, "message": "Oh hi! I have some news for you!"}'
+curl http://localhost:8888/api/message/new -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "sender_id": 123456, "message": "Oh hi! I have some news for you!"}' | jq .
 
 # insert message
-curl http://localhost:8888/api/message/new -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "sender_id": 123, "message": "I am curious, tell me..."}'
+curl http://localhost:8888/api/message/new -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "sender_id": 123, "message": "I am curious, tell me..."}' | jq .
 
 # insert message
-curl http://localhost:8888/api/message/new -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "sender_id": 123456, "message": "We are not real... :-("}'
+curl http://localhost:8888/api/message/new -X 'POST' -H 'Content-Type: application-json' --data '{"chat_id": 987, "sender_id": 123456, "message": "We are not real... :-("}' | jq .
